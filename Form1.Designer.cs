8df2016f641ce208f46d6ebd5ff257be6c826153
Edward <gulyaev.ed.v@gmail.com>
2021-06-21 22:57:37 +0400

    ChooseFolder

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,10 +37,19 @@ namespace Observer
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.btnClean = new System.Windows.Forms.Button();
+            this.tbFilter = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblLinkPath = new System.Windows.Forms.LinkLabel();
+            this.lblPath = new System.Windows.Forms.Label();
             this.stripMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -65,7 +74,7 @@ namespace Observer
             this.tbTerminal.Name = "tbTerminal";
             this.tbTerminal.ReadOnly = true;
             this.tbTerminal.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbTerminal.Size = new System.Drawing.Size(784, 552);
+            this.tbTerminal.Size = new System.Drawing.Size(784, 432);
             this.tbTerminal.TabIndex = 0;
             this.tbTerminal.TabStop = false;
             this.tbTerminal.Text = "\r\n";
@@ -74,7 +83,7 @@ namespace Observer
             // 
             this.stripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.helpToolStripMenuItem});
+            this.aboutToolStripMenuItem});
             this.stripMenu.Location = new System.Drawing.Point(0, 0);
             this.stripMenu.Name = "stripMenu";
             this.stripMenu.Size = new System.Drawing.Size(784, 24);
@@ -106,19 +115,11 @@ namespace Observer
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
             this.exitToolStripMenuItem.Text = "&Exit";
             // 
-            // helpToolStripMenuItem
-            // 
-            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aboutToolStripMenuItem});
-            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
-            this.helpToolStripMenuItem.Text = "&About";
-            // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
-            this.aboutToolStripMenuItem.Text = "&About...";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "&About";
             // 
             // timer
             // 
@@ -129,7 +130,7 @@ namespace Observer
             // btnClean
             // 
             this.btnClean.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btnClean.Location = new System.Drawing.Point(0, 581);
+            this.btnClean.Location = new System.Drawing.Point(0, 569);
             this.btnClean.Name = "btnClean";
             this.btnClean.Size = new System.Drawing.Size(784, 23);
             this.btnClean.TabIndex = 2;
@@ -137,11 +138,118 @@ namespace Observer
             this.btnClean.UseVisualStyleBackColor = true;
             this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
             // 
+            // tbFilter
+            // 
+            this.tbFilter.Enabled = false;
+            this.tbFilter.Location = new System.Drawing.Point(48, 464);
+            this.tbFilter.Name = "tbFilter";
+            this.tbFilter.Size = new System.Drawing.Size(100, 23);
+            this.tbFilter.TabIndex = 3;
+            this.tbFilter.Text = "*.xlsx";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(48, 496);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(200, 496);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 19);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(48, 520);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(83, 19);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(200, 520);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(83, 19);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(48, 544);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(83, 19);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(200, 544);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(83, 19);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 464);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Filter";
+            // 
+            // lblLinkPath
+            // 
+            this.lblLinkPath.AutoSize = true;
+            this.lblLinkPath.Location = new System.Drawing.Point(200, 464);
+            this.lblLinkPath.Name = "lblLinkPath";
+            this.lblLinkPath.Size = new System.Drawing.Size(28, 15);
+            this.lblLinkPath.TabIndex = 11;
+            this.lblLinkPath.TabStop = true;
+            this.lblLinkPath.Text = "C:\\\\";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(160, 464);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(31, 15);
+            this.lblPath.TabIndex = 12;
+            this.lblPath.Text = "Path";
+            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 604);
+            this.ClientSize = new System.Drawing.Size(784, 592);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lblLinkPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.tbFilter);
             this.Controls.Add(this.btnClean);
             this.Controls.Add(this.tbTerminal);
             this.Controls.Add(this.stripMenu);
@@ -163,10 +271,20 @@ namespace Observer
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.TextBox tbFilter;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.LinkLabel lblLinkPath;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,24 +8,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.Json;
 
 namespace Observer
 {
     public partial class Form : System.Windows.Forms.Form
     {
         public string Terminal { get; set; }
+        public string ChooseFolder { get; set; }
+        public string Filter { get; set; }
         public Form()
         {
             InitializeComponent();
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            aboutToolStripMenuItem.Click += AboutToolStripMenuItemOnClick;
+            exitToolStripMenuItem.Click += ExitToolStripMenuItemOnClick;
+            Filter = tbFilter.Text;
+            
+        }
+
+        private void ExitToolStripMenuItemOnClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AboutToolStripMenuItemOnClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("About");
         }
 
         private void Watcher()
         {
             try
             {
-                var watcher = new FileSystemWatcher(@"C:\Users\Dante\Desktop\ivent");
+                var watcher = new FileSystemWatcher(ChooseFolder);
                 Observer observ = new Observer();
                 observ.Notify += DisplayMessage;
                 watcher.NotifyFilter = NotifyFilters.Attributes
@@ -43,7 +61,7 @@ namespace Observer
                 watcher.Renamed += observ.OnRenamed;
                 watcher.Error += observ.OnError;
 
-                watcher.Filter = "*.xlsx";
+                watcher.Filter = Filter;
                 watcher.IncludeSubdirectories = true;
                 watcher.EnableRaisingEvents = true;
 
@@ -57,7 +75,7 @@ namespace Observer
 
         async void WatcherAcync()
         {
-            await Task.Run(() => Watcher());
+            //await Task.Run(() => Watcher());
         }
 
         private void DisplayMessage(string message)
@@ -67,12 +85,24 @@ namespace Observer
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            WatcherAcync();
+            Watcher();
         }
 
         private void btnClean_Click(object sender, EventArgs e)
         {
             tbTerminal.Clear();
+        }
+
+
+        private async void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                ChooseFolder = fbd.SelectedPath;
+                lblLinkPath.Text = ChooseFolder;
+
+            }
         }
     }
 }
